9c40125391a5c3557d18c30dd925a2d22720ceb7
Ramoy Roberts <rroberts20j@vtdi.edu.jm>
2022-05-07 19:53:10 -0500

    Add project files.

---
+++ PLUM.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PLUM.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
